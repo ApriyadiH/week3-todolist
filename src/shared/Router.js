26ec9411fe83caa45e2c57dc4ee29b4c5f2f2fe3
Apriyadi Hegarto <ahegarto@gmail.com>
2022-11-03 05:09:16 +0700

    balik ke hash

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/layout";
 import Page from "../components/page";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/:id" element={<Page />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
